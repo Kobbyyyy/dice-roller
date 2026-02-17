93451e5a48af5a2f8ca128c072fbd78706c8f651
Nana Kwabena Inkum <akotoinkumkobby@gmail.com>
2026-02-17 15:34:50 -0600

    Update index.html and script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+var rollHistory = [];
+
+function rollDice() { 
+    var diceValue = Math.floor(Math.random() * 6) + 1;
+
+    document.getElementById("diceResult").value = diceValue;
+
+    if (diceValue===6) {
+        document.getElementById("message").innerHTML = "You rolled a 6! You get another turn!";
+    } else {
+        document.getElementById("message").innerHTML =  ". Try again!";
+
+        rollHistory.push(diceValue);
+        if (rollHistory.length > 3) {
+            rollHistory.shift();
+        }
+
+        document.getElementById("rollHistory").innerHTML = rollHistory.join(" - ");
+
+    }
+}
+
